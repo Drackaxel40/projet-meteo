804ca8046099622b9b554bc69de713d96112b38f
Axel M. Soares <150051319+AxelMSoares@users.noreply.github.com>
2024-08-07 14:57:10 +0200

    Done some css  and html changes. Put langage and units in the conf

--- api/fetchWeather.js
+++ api/fetchWeather.js
@@ -1,7 +1,7 @@
 export default async function fetchWeather() {
     const config = await fetch ("../config/conf.json").then((res) => res.json());
 
-    const apiUrl = `${config.weather_api_url}?lat=${config.lat}&lon=${config.lon}&appid=${config.weather_api_key}`;
+    const apiUrl = `${config.weather_api_url}?lat=${config.lat}&lon=${config.lon}&appid=${config.weather_api_key}&lang=${config.lang}&units=${config.units}`;
     
     try{
         const response = await fetch(apiUrl);
--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 import fetchWeather from "../api/fetchWeather.js";
 
+const weatherData = await fetchWeather();
+
+console.log(weatherData);
+
 const cityName= document.querySelector(".city-name");
-cityName.textContent = "Loading...";
+cityName.textContent = weatherData.name;
+
+const temp = document.querySelector(".temp");
+temp.textContent = weatherData.main.temp + " °C";
+
+const feelsLike = document.querySelector(".feels-like");
+feelsLike.textContent = "Ressenti: " + weatherData.main.feels_like + " °C";
+
+const tempMax = document.querySelector(".temp-max");
+tempMax.textContent = "Temp. maximale: " + weatherData.main.temp_max + " °C";
+
+const tempMin = document.querySelector(".temp-min");
+tempMin.textContent = "Temp. minimale: " + weatherData.main.temp_min + " °C";
+
+const weather = document.querySelector(".weather");
+weather.textContent = weatherData.weather[0].description;
+
+const humidity = document.querySelector(".humidity");
+humidity.textContent = "Humidité: " + weatherData.main.humidity + " %";
+
+const icon = document.querySelector(".icon");
+icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
